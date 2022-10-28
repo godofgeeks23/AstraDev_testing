459db0e318060692e6e9721bdd6087ba9d475b36
Aviral Srivastava <aviralji4@gmail.com>
2022-10-28 20:26:27 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+
+const App = () => {
+    return (
+        <div className='h-100 d-flex align-items-center justify-content-center'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
