ad7ab47d1b4b31360e2dbe72ae77fa58e24d6cb1
Aviral Srivastava <aviralji4@gmail.com>
2022-11-06 01:05:45 +0530

    Update user.js

--- server_alt/models/user.js
+++ server_alt/models/user.js
@@ -1,106 +1,137 @@
-var mongoose=require('mongoose');
-const jwt=require('jsonwebtoken');
-const bcrypt=require('bcrypt');
-const confiq=require('../config/config').get(process.env.NODE_ENV);
-const salt=10;
+var mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const confiq = require('../config/config').get(process.env.NODE_ENV);
+const validator = require('validator');
+const salt = 10;
 
-const userSchema=mongoose.Schema({
-    firstname:{
-        type: String,
-        required: true,
-        maxlength: 100
-    },
-    lastname:{
-        type: String,
-        required: true,
-        maxlength: 100
-    },
-    email:{
-        type: String,
-        required: true,
-        trim: true,
-        unique: 1
-    },
-    password:{
-        type:String,
-        required: true,
-        minlength:8
-    },
-    password2:{
-        type:String,
-        required: true,
-        minlength:8
+// const userSchema = mongoose.Schema({
+//     firstname: {
+//         type: String,
+//         required: true,
+//         maxlength: 100
+//     },
+//     lastname: {
+//         type: String,
+//         required: true,
+//         maxlength: 100
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         unique: 1
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         minlength: 8
+//     },
+//     password2: {
+//         type: String,
+//         required: true,
+//         minlength: 8
 
+//     },
+//     token: {
+//         type: String
+//     }
+// });
+
+
+const userSchema = new mongoose.Schema({
+
+    fname: { type: String, required: true },
+    mname: { String, required: false },
+    lname: { type: String, required: true },
+    username: { type: String, required: true, trim: true, unique: true },
+    email: {
+        type: String, unique: true, required: true, trim: true, lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
     },
-    token:{
-        type: String
-    }
-});
+    password: { type: String, required: true },
+    password2: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    createdDate: { type: Date, default: Date.now },
+
+    pswd_created: { type: Date, default: Date.now },
+    status: { type: Boolean, default: true },
+    cust_id: { type: String, default: null },
+    token: { type: String, default: null },
+    role_id: { type: String, default: null },
+
+})
+
+
 // to signup a user
-userSchema.pre('save',function(next){
-    var user=this;
-    
-    if(user.isModified('password')){
-        bcrypt.genSalt(salt,function(err,salt){
-            if(err)return next(err);
+userSchema.pre('save', function (next) {
+    var user = this;
 
-            bcrypt.hash(user.password,salt,function(err,hash){
-                if(err) return next(err);
-                user.password=hash;
-                user.password2=hash;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(salt, function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                user.password2 = hash;
                 next();
             })
 
         })
     }
-    else{
+    else {
         next();
     }
 });
 
 //to login
-userSchema.methods.comparepassword=function(password,cb){
-    bcrypt.compare(password,this.password,function(err,isMatch){
-        if(err) return cb(next);
-        cb(null,isMatch);
+userSchema.methods.comparepassword = function (password, cb) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+        if (err) return cb(next);
+        cb(null, isMatch);
     });
 }
 
 // generate token
 
-userSchema.methods.generateToken=function(cb){
-    var user =this;
-    var token=jwt.sign(user._id.toHexString(),confiq.SECRET);
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), confiq.SECRET);
 
-    user.token=token;
-    user.save(function(err,user){
-        if(err) return cb(err);
-        cb(null,user);
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
     })
 }
 
 // find by token
-userSchema.statics.findByToken=function(token,cb){
-    var user=this;
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
 
-    jwt.verify(token,confiq.SECRET,function(err,decode){
-        user.findOne({"_id": decode, "token":token},function(err,user){
-            if(err) return cb(err);
-            cb(null,user);
+    jwt.verify(token, confiq.SECRET, function (err, decode) {
+        user.findOne({ "_id": decode, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user);
         })
     })
 };
 
 //delete token
 
-userSchema.methods.deleteToken=function(token,cb){
-    var user=this;
+userSchema.methods.deleteToken = function (token, cb) {
+    var user = this;
 
-    user.update({$unset : {token :1}},function(err,user){
-        if(err) return cb(err);
-        cb(null,user);
+    user.update({ $unset: { token: 1 } }, function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
     })
 }
 
 
-module.exports=mongoose.model('User',userSchema);
+module.exports = mongoose.model('schema_beta_user', userSchema);
