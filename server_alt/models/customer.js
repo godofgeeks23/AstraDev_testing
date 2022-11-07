c2d2e1e507d0870de52226871cda0c1cef2986ce
Aviral Srivastava <aviralji4@gmail.com>
2022-11-07 23:39:12 +0530

    Update customer.js

--- server_alt/models/customer.js
+++ server_alt/models/customer.js
@@ -4,14 +4,12 @@ const validator = require('validator');
 const customer = new mongoose.Schema({
 
   createdDate: { type: Date, default: Date.now },
-
   cust_id: { type: String, default: null },
   cname : { type: String, required: true },
   status: { type: Boolean, default: true },
-  cname : { type: String, required: true },
 
 })
 
-const model = mongoose.model("schema_alpha_customer", customer)
+const model = mongoose.model("schema_beta_customer", customer)
 
 module.exports = model
