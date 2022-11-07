6aa709e80ea5356c0279d99ff91876de779c5e40
Aviral Srivastava <aviralji4@gmail.com>
2022-11-07 18:08:56 +0530

    Update Dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,87 +2,75 @@ import React, { useEffect, useState } from 'react'
 import { isExpired, decodeToken } from "react-jwt";
 import { useNavigate } from 'react-router-dom';
 import '../App.css'
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Cookies from 'js-cookie';
 
 
-const logout = () => {
-    localStorage.removeItem('token');
-    window.location.href = '/login'
-  };
+async function logout() {
+  const res = await fetch('http://localhost:3000/api/logout', {
+    "headers": {
+      // "cookies":"eyJhbGciOiJIUzI1NiJ9.NjM2NmFiMmU2YTcwZmMwYTEwMTQ1NDQy.YCU80QSzCkkC6KlsIpq4HRL2wSek6C2vGbTAfZs3Im4"
+      "cookies": Cookies.get('auth')
+    }
+  })
+  localStorage.removeItem('token');
+  Cookies.remove('auth')
+  alert("Logged out! Redirecting to Login page...")
+  window.location.href = '/login'
+};
 
 const Dashboard = () => {
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const [quote, setQuote] = useState('')
+  const [quote, setQuote] = useState('')
 
-    async function populateQuote() {
+  async function populateQuote() {
 
-        const res = await fetch('http://localhost:1337/api/quote', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                'xaccesstoken': localStorage.getItem('token')
-            })
-        })
+    const res = await fetch('http://localhost:3000/api/profile', {
+      "headers": {
+        // "cookies":"eyJhbGciOiJIUzI1NiJ9.NjM2NmFiMmU2YTcwZmMwYTEwMTQ1NDQy.YCU80QSzCkkC6KlsIpq4HRL2wSek6C2vGbTAfZs3Im4"
+        "cookies": Cookies.get('auth')
+      }
+    })
+    const data = await res.json();
 
-        console.log("req sent from dashobard to api", localStorage.getItem('token'))
-        const data = await res.json()
-        console.log("received data from API ")
+    if (data.isAuth) {
+      setQuote(data.name)
+    }
+    else {
+      // alert(data.error)
+      alert("You are not logged in... Navigating to /login")
 
-        if (data.status == 'ok') {
-            setQuote(data.quote)
-        }
-        else {
-            alert(data.error)
-        }
-
+      window.location.href = '/login'
     }
 
-    useEffect(() => {
+  }
+  
+  populateQuote()
 
-        const token = localStorage.getItem('token')
-        if (token) {
-            const user = decodeToken(token);
+  return (<div className='dashboardbody end-0 start-0 top-0 position-absolute'>
 
-            if (!user) {
-                localStorage.removeItem('token')
-
-                navigate('/login')
-            }
-            else {
-                populateQuote()
-            }
-
-        }
-    }, [])
-
-    return (<div className='dashboardbody end-0 start-0 top-0 position-absolute'>
-
-<Navbar bg="dark" variant="dark" expand="lg" className=''>
+    <Navbar bg="dark" variant="dark" expand="lg" className=''>
       <Container>
         <Navbar.Brand href="#home">Astra</Navbar.Brand>
         {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
         {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Welcome, {quote}</Nav.Link>
-            <Nav.Link href="#home">Assets</Nav.Link>
-            <Nav.Link href="#link">Team</Nav.Link>
-            <Nav.Link href="#link">Invite</Nav.Link>
-            <Nav.Link onClick={logout}>Logout</Nav.Link>
-          </Nav>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Welcome, {quote}</Nav.Link>
+          <Nav.Link href="#home">Assets</Nav.Link>
+          <Nav.Link href="#link">Team</Nav.Link>
+          <Nav.Link href="#link">Invite</Nav.Link>
+          <Nav.Link onClick={logout}>Logout</Nav.Link>
+        </Nav>
         {/* </Navbar.Collapse> */}
       </Container>
     </Navbar>
 
-        {/* <button onClickCapture={logout}>logout user</button> */}
-    </div>)
+    {/* <button onClickCapture={logout}>logout user</button> */}
+  </div>)
 }
 
 export default Dashboard
