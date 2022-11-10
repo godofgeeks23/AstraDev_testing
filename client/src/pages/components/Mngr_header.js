1a7c057a936d08c58812089b828f78097d62b6e7
Aviral Srivastava <aviralji4@gmail.com>
2022-11-10 20:50:23 +0530

    Create Mngr_header.js

---
+++ client/src/pages/components/Mngr_header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function mngr_header() {
+  return (
+      <h1>This section is visible on the Manager role dashbaord only!</h1>
+  );
+}
+
+export default mngr_header;
