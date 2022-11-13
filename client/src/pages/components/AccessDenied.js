9903db222556829de63da39331fd77cb9ecbd153
Aviral Srivastava <aviralji4@gmail.com>
2022-11-13 23:17:50 +0530

    Create AccessDenied.js

---
+++ client/src/pages/components/AccessDenied.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { isExpired, decodeToken } from "react-jwt";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function access_denied() {
+  return (
+      <h1>You are not authorized for accessing this page!</h1>
+  );
+}
+
+export default access_denied;
