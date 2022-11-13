eb0ab0fbcb8fd22925461e28a927a8791c1aa7ff
Aviral Srivastava <aviralji4@gmail.com>
2022-11-13 23:18:03 +0530

    Update customer.js

--- server_alt/models/customer.js
+++ server_alt/models/customer.js
@@ -4,10 +4,10 @@ const validator = require('validator');
 const customer = new mongoose.Schema({
 
   createdDate: { type: Date, default: Date.now },
-  cust_id: { type: String, default: null },
+  cust_id: { type: String, default : null  },
   cname : { type: String, required: true },
   status: { type: Boolean, default: true },
-  is_sec_researcher: { type: Boolean, default: false},
+  is_sec_provider: { type: Boolean, default: false},
   is_customer: { type: Boolean, default: true},
 
 })
