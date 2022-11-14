c4ea9a1ad55c8d13390f947b04b9cd9cabe9464e
Aviral Srivastava <aviralji4@gmail.com>
2022-11-14 14:21:29 +0530

    Update user.js

--- server_alt/models/user.js
+++ server_alt/models/user.js
@@ -5,40 +5,6 @@ const confiq = require('../config/config').get(process.env.NODE_ENV);
 const validator = require('validator');
 const salt = 10;
 
-// const userSchema = mongoose.Schema({
-//     firstname: {
-//         type: String,
-//         required: true,
-//         maxlength: 100
-//     },
-//     lastname: {
-//         type: String,
-//         required: true,
-//         maxlength: 100
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         unique: 1
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         minlength: 8
-//     },
-//     password2: {
-//         type: String,
-//         required: true,
-//         minlength: 8
-
-//     },
-//     token: {
-//         type: String
-//     }
-// });
-
-
 const userSchema = new mongoose.Schema({
 
     fname: { type: String, required: true },
