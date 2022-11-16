f7124f87b1a1d1a1cc81b17f02919ff24e4b32c1
Aviral Srivastava <aviralji4@gmail.com>
2022-11-16 23:34:39 +0530

    Update comment.js

--- server_alt/models/comment.js
+++ server_alt/models/comment.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 const comment = new mongoose.Schema({
 
   createdDate: { type: Date, default: Date.now },
-  user_id: { type: String, default : null  },
-  content : { type: String, required: true },
+  user_id: { type: String, default: null },
+  content: { type: String, required: true },
   media: { type: String, default: null },
-  upvotes: { type: Number, default: 0},
-  downvotes: { type: Number, default: 0},
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 },
 
 })
 
