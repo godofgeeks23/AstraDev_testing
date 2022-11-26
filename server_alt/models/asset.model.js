b2abfb318933881500a8aed775c233ca5ec1389f
Aviral Srivastava <aviralji4@gmail.com>
2022-11-26 23:08:28 +0530

    Update asset.model.js

--- server_alt/models/asset.model.js
+++ server_alt/models/asset.model.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose')
 const validator = require('validator');
 
 const manager = new mongoose.Schema({
-    mngr_id: { type: String, required: true }
-  });
+  mngr_id: { type: String, required: true }
+});
 
 const asset = new mongoose.Schema({
 
-    title: { type: String, required: true },
-    type: { type: String, required: true },
-    asset_id: { type: String, required: true, unique: true },
-    target: { type: String, required: true },
-    created_date: { type: Date, default: Date.now },
-    description: { type: String, default: null },
-    assignor_managers: { type: [manager], required: true},
-    assignee_managers: { type: [manager], required: true},
+  title: { type: String, required: true },
+  type: { type: String, required: true },
+  asset_id: { type: String, required: true, unique: true },
+  target: { type: String, required: true },
+  created_date: { type: Date, default: Date.now },
+  description: { type: String, default: null },
+  assignor_managers: { type: [manager], required: true },
+  assignee_managers: { type: [manager], required: true },
 
 })
 
