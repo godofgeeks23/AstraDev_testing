bd4bc5760ad7872ebab24bdee90a42fa803765a8
Aviral Srivastava <aviralji4@gmail.com>
2022-11-26 23:08:32 +0530

    Update comment.js

--- server_alt/models/comment.js
+++ server_alt/models/comment.js
@@ -6,7 +6,7 @@ const comment = new mongoose.Schema({
   user_id: { type: String, default: null },
   content: { type: String, required: true },
   media: { type: String, default: null },
-  vuln_id: { type: String, required: true},
+  vuln_id: { type: String, required: true },
   activity_id: { type: String, required: true },
   fname: { type: String, required: true },
   lname: { type: String, required: true },
