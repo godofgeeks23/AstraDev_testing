e0445fa76e78959c67b51b7a917d0896a8ae024d
Aviral Srivastava <aviralji4@gmail.com>
2022-11-26 23:08:39 +0530

    Update roles.js

--- server_alt/models/roles.js
+++ server_alt/models/roles.js
@@ -4,7 +4,7 @@ const validator = require('validator');
 const role = new mongoose.Schema({
 
   role_name: { type: String, required: true },
-  role_id: { type: String, default : null},
+  role_id: { type: String, default: null },
   createdDate: { type: Date, default: Date.now },
   status: { type: Boolean, default: true },
 
