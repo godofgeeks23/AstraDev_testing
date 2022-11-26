6abe0f89fa3e0eca04c565ba92d5ea094da2451a
Aviral Srivastava <aviralji4@gmail.com>
2022-11-26 23:08:46 +0530

    Update test_queries.js

--- server_alt/test_queries.js
+++ server_alt/test_queries.js
@@ -20,19 +20,19 @@ app.use(express.json())
 mongoose.connect(process.env.DB_CONNECT, () => console.log("connected to DB"));
 
 async function get_results() {
-    // res_asset = await asset.find()
-    // console.log(res_asset);
+  // res_asset = await asset.find()
+  // console.log(res_asset);
 
-    // var newrole = new role({
-    //     role_name: "Employee",
-    //     role_id: "jh32bjh3b2j265b7",
-    // })
-    // await newrole.save()
+  // var newrole = new role({
+  //     role_name: "Employee",
+  //     role_id: "jh32bjh3b2j265b7",
+  // })
+  // await newrole.save()
 
-    const newrole = await role.create({
-        role_name: "Employee",
-        role_id: "jh32bjh3b2j265b7",
-      });
+  const newrole = await role.create({
+    role_name: "Employee",
+    role_id: "jh32bjh3b2j265b7",
+  });
 }
 
 get_results();
