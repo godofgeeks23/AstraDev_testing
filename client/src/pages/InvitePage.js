86654a87707c8959f409d338e18ee6bf802ef253
Aviral Srivastava <aviralji4@gmail.com>
2022-12-12 07:58:08 +0530

    Update InvitePage.js

--- client/src/pages/InvitePage.js
+++ client/src/pages/InvitePage.js
@@ -7,47 +7,71 @@ import Form from 'react-bootstrap/Form';
 
 const InvitePage = (props) => {
 
-    const [hasAccess, setHasAccess] = useState(null);
-    
-    const [email, setEmail] = useState('')
-    const [role, setRole] = useState('')
-    
-    async function chkAccess() {
+	const [hasAccess, setHasAccess] = useState(null);
 
-        const res = await fetch('http://localhost:3000/api/profile', {
-            "headers": { "cookies": Cookies.get('auth') }
-        })
-        const data = await res.json();
-        if (data.isAuth)
-            if (props.allowedRoles.includes(data.role_name))
-                return true;
-        return false;
+	const [email, setEmail] = useState('')
+	const [role, setRole] = useState('')
 
-    }
-    useEffect(() => {
-        chkAccess().then((result) => {
-            setHasAccess(!!result);
-        });
-    }, []);
+	async function chkAccess() {
 
-    console.log(props.allowedRoles[0])
-    if (hasAccess === null) return (<div>Loading</div>);
-    if (!hasAccess) {
-        return <Access_denied />;
-    }
+		const res = await fetch('http://localhost:3000/api/profile', {
+			"headers": { "cookies": Cookies.get('auth') }
+		})
+		const data = await res.json();
+		if (data.isAuth)
+			if (props.allowedRoles.includes(data.role_name))
+				return true;
+		return false;
 
+	}
+	useEffect(() => {
+		chkAccess().then((result) => {
+			setHasAccess(!!result);
+		});
+	}, []);
 
+	console.log(props.allowedRoles[0])
+	if (hasAccess === null) return (<div>Loading</div>);
+	if (!hasAccess) {
+		return <Access_denied />;
+	}
+
+	async function sendInviteMail(dest, token) {
+		console.log("invite sending fnction called...")
+		const req_body = {
+			source: "support@cyethack.com",
+			destinations: [dest],
+			subject: "Astra Onboarding Invitation",
+			body: "Hello from cyethack!",
+			html: "<h1>Visit http://localhost:3001/activateuser?token=" + token + " to initiate onboarding!<h1>",
+		}
+		const response = await fetch('https://qet85fubbi.execute-api.ap-south-1.amazonaws.com/dev/sendemail', {
+			method: 'POST',
+			// credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(req_body)
+		})
+		const data = await response.json()
+		console.log("got response: ", data)
+		if (data.message_id) {
+			alert("Check Your Mail!")
+		} else {
+			alert("Unable to send mail!")
+		}
+	}
 
 	async function inviteUser(event) {
 
 		event.preventDefault()
 
-        const req_body = {
-                email  : email,
-                role_id : role,
-                invited_by : "63762ddbb7e0efa6b95168cd",
-                validity: "24"
-        }
+		const req_body = {
+			email: email,
+			role_id: role,
+			invited_by: "63762ddbb7e0efa6b95168cd",
+			validity: "24"
+		}
 		const response = await fetch('http://localhost:3000/api/create_pending_user', {
 			method: 'POST',
 			credentials: 'include',
@@ -60,7 +84,8 @@ const InvitePage = (props) => {
 		console.log(data)
 
 		if (data.status == "ok") {
-			alert("Invite Sent Successfully!\nVisit http://localhost:3001/activateuser?token=" + data.pending_user_id + " to activate the user!")
+			// alert("Invite Sent Successfully!\nVisit http://localhost:3001/activateuser?token=" + data.pending_user_id + " to activate the user!")
+			await sendInviteMail(email, data.pending_user_id)
 		} else {
 			alert("Invitation Failed!")
 		}
@@ -68,27 +93,27 @@ const InvitePage = (props) => {
 		// console.log(data)
 	}
 
-    if (hasAccess)
-        return (
-            <div className='login_container p-5 m-5 bg-dark'>
-			<h1 className='text-center p-3'>Invite User</h1>
-			<Form onSubmit={inviteUser}>
-				<Form.Group className="mb-3" controlId="formBasicEmail">
-					<Form.Label>Email address</Form.Label>
-					<Form.Control type="text" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
-				</Form.Group>
+	if (hasAccess)
+		return (
+			<div className='login_container p-5 m-5 bg-dark'>
+				<h1 className='text-center p-3'>Invite User</h1>
+				<Form onSubmit={inviteUser}>
+					<Form.Group className="mb-3" controlId="formBasicEmail">
+						<Form.Label>Email address</Form.Label>
+						<Form.Control type="text" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+					</Form.Group>
 
-				<Form.Group className="mb-3" controlId="formBasicRole">
-					<Form.Label>Role</Form.Label>
-					<Form.Control type="text" placeholder="Role" value={role} onChange={(e) => setRole(e.target.value)} />
-				</Form.Group>
-				<Button variant="primary" type="submit">
-					Submit
-				</Button>
-			</Form>
+					<Form.Group className="mb-3" controlId="formBasicRole">
+						<Form.Label>Role</Form.Label>
+						<Form.Control type="text" placeholder="Role" value={role} onChange={(e) => setRole(e.target.value)} />
+					</Form.Group>
+					<Button variant="primary" type="submit">
+						Submit
+					</Button>
+				</Form>
 
-		</div>
-        );
+			</div>
+		);
 
 }
 
