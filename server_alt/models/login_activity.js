c39bb456fad3503753c7698552fbe045e78d6dd2
Aviral Srivastava <aviralji4@gmail.com>
2022-12-30 10:33:14 +0530

    Upgraded schema for location details

--- server_alt/models/login_activity.js
+++ server_alt/models/login_activity.js
@@ -8,8 +8,30 @@ const login_activity = new mongoose.Schema({
     host: { type: String, required: true },    
     browser: { type: String, required: true },
     status: { type: Boolean, default: true }, // true - login success, false - login failed
-    location: { type: String, default: null }, // location of the user
-
+    city: { type: String, default: null},
+    region: { type: String, default: null},
+    region_code: { type: String, default: null},
+    country: { type: String, default: null},
+    country_name: { type: String, default: null},
+    country_code: { type: String, default: null},
+    country_code_iso3: { type: String, default: null},
+    country_capital: { type: String, default: null},
+    country_tld: { type: String, default: null},
+    continent_code: { type: String, default: null},
+    in_eu: { type: String, default: null},
+    postal: { type: String, default: null},
+    latitude: { type: String, default: null},
+    longitude: { type: String, default: null},
+    timezone: { type: String, default: null},
+    utc_offset: { type: String, default: null},
+    country_calling_code: { type: String, default: null},
+    currency: { type: String, default: null},
+    currency_name: { type: String, default: null},
+    languages: { type: String, default: null},
+    country_area: { type: String, default: null},
+    country_population: { type: String, default: null},
+    asn: { type: String, default: null},
+    org: { type: String, default: null},
 })
 
 const model = mongoose.model("login_activity", login_activity)
