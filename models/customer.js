fddbec885f4858f9ed0fb1620a7ba68591582f65
Aviral Srivastava <aviralji4@gmail.com>
2023-01-11 22:30:42 +0530

    Update customer.js

--- models/customer.js
+++ models/customer.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 
 const customer = new mongoose.Schema({
 
-  createdDate: { type: Date, default: Date.now },
-  cname: { type: String, required: true },
-  status: { type: Boolean, default: true },
-  is_sec_provider: { type: Boolean, default: false },
-  is_customer: { type: Boolean, default: true },
-  profile_image: { type: String, default: null },
+  createdDate: { type: Date, default: Date.now }, // date of creation of the customer
+  cname: { type: String, required: true }, // customer name
+  status: { type: Boolean, default: true }, // status of the customer - active 1 or inactive 0
+  is_sec_provider: { type: Boolean, default: false }, // is the customer a secondary provider
+  is_customer: { type: Boolean, default: true }, // is the customer a customer
+  profile_image: { type: String, default: null }, // profile image of the customer
 
 })
 
