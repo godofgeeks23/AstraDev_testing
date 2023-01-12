b3844c72214f8614a1f6a1365ff5b656a038b6ca
Aviral Srivastava <aviralji4@gmail.com>
2023-01-12 23:04:20 +0530

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose')
 
 const comment = new mongoose.Schema({
 
-  createdDate: { type: Date, default: Date.now },
-  user_id: { type: String, default: null },
-  content: { type: String, required: true },
-  media: { type: String, default: null },
-  vuln_id: { type: String, required: true },
-  activity_id: { type: String, required: true },
-  fname: { type: String, required: true },
-  lname: { type: String, required: true },
-  username: { type: String, required: true },
-  email: { type: String },
-  cust_id: { type: String, default: null },
-  role_id: { type: String, default: null },
-  cname: { type: String },
+  createdDate: { type: Date, default: Date.now }, // date of creation of the comment
+  user_id: { type: String, default: null },   // user id of the user who created the comment
+  content: { type: String, required: true },  // content of the comment
+  media: { type: String, default: null }, // media of the comment
+  vuln_id: { type: String, required: true }, // vulnerability id of the comment
+  activity_id: { type: String, required: true },  // activity id of the comment
+  fname: { type: String, required: true },  // first name of the user who created the comment
+  lname: { type: String, required: true },  // last name of the user who created the comment
+  username: { type: String, required: true }, // username of the user who created the comment
+  email: { type: String }, // email of the user who created the comment
+  cust_id: { type: String, default: null },  // customer id of the user who created the comment
+  role_id: { type: String, default: null },  // role id of the user who created the comment
+  cname: { type: String },  // customer name of the user who created the comment
 
 })
 
